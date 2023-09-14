3aefae51c8ee84361facbf1795510c7a5399ac31
kundan kumar sahu <kundanlal96580@gmail.com>
2023-09-14 21:40:22 +0530

    commit

--- clientapp/src/component/Navbar/Navbar.jsx
+++ clientapp/src/component/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ import {Link} from "react-router-dom"
 
 function Navbar() {
 
+
   const [username , SetUsername] = useState(null);
   useEffect(() => {
     fetch('http://localhost:8000/profile', {
@@ -33,7 +34,6 @@ function Navbar() {
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
-
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                         { username && (
